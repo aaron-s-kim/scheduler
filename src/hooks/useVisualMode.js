24ca41a8812ae1a1677219c1598469f6d647f0bd
Aaron K <tonberryspawn@gmail.com>
2021-12-11 19:11:11 -0800

    W7D5 Building a Hook - adding back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,9 +2,26 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] =  useState(initial);
-  
-  const transition = (mode) => setMode(mode);
+  const [history, setHistory] = useState([initial]);
+  console.log('__start hook__');
   console.log('mode', mode);
+  console.log('history', history);
 
-  return { mode, transition };
+  // When transition is called, we need to add the new mode to our history.
+  const transition = (mode) => {
+    const newHistory = [...history];
+    newHistory.push(mode);
+    setHistory(newHistory);
+    setMode(mode);
+  }
+  
+  // When back is called, we should set the mode to the previous item in our history array.
+  const back = () => {
+    history.pop(); // removes last item
+    const lastItem = history[history.length - 1] // previous last item
+    console.log('lastItem', lastItem);
+    setMode(lastItem);
+  };
+
+  return { mode, transition, back };
 }
